3f8bd79507abb7820211375df6e48f677581d603
MinhND <minh1996bk@gmail.com>
2023-10-27 23:32:54 +0700

    dashboar

--- app/containers/DashBoardPage/DashBoardTask.js
+++ app/containers/DashBoardPage/DashBoardTask.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, Button} from 'native-base';
-import Icon from 'react-native-vector-icons/Ionicons'
+import React, {useState, useEffect} from 'react';
+import {Text, View, Button} from 'native-base';
+import Icon from 'react-native-vector-icons/Ionicons';
 import LoadingLayout from 'components/LoadingLayout';
-import { navigate } from '../../RootNavigation';
-import { TouchableOpacity } from 'react-native';
-import { getCoutTask } from '../../api/tasks';
-
+import {navigate} from '../../RootNavigation';
+import {TouchableOpacity} from 'react-native';
+import {getCoutTask} from '../../api/tasks';
 
 function split(array, n) {
   let [...arr] = array;
@@ -16,102 +15,119 @@ function split(array, n) {
   return res;
 }
 
-export default DashBoardTask = (props) => {
-  const { kanbanTaskConfigs, query, navigation } = props;
+export default DashBoardTask = props => {
+  const {kanbanTaskConfigs, query, navigation} = props;
   const [reportTask, setReportTask] = useState({});
   const [loading, setLoading] = useState();
-  const [configs, setConfigs] = useState([])
+  const [configs, setConfigs] = useState([]);
   useEffect(() => {
     query && handleGetTask();
   }, [query]);
 
   useEffect(() => {
     if (Array.isArray(kanbanTaskConfigs)) {
-      const data = split(kanbanTaskConfigs, 3)
+      const data = split(kanbanTaskConfigs, 3);
 
       if (data.length > 1 && data[data.length - 1].length === 1) {
-        data[data.length - 1].push(data[data.length - 2][2])
-        data[data.length - 2].pop()
+        data[data.length - 1].push(data[data.length - 2][2]);
+        data[data.length - 2].pop();
       }
 
-      setConfigs(data)
+      setConfigs(data);
     }
   }, [kanbanTaskConfigs]);
 
   useEffect(() => {
-    navigation.addListener(
-      'focus', () => {
-        handleGetTask()
-      }
-    );
-
+    navigation.addListener('focus', () => {
+      handleGetTask();
+    });
   }, []);
 
   const handleGetTask = async () => {
-    setLoading(true)
-    const { startDate, endDate, organizationUnitId, employeeId } = query
+    setLoading(true);
+    const {startDate, endDate, organizationUnitId, employeeId} = query;
     const newQuery = {};
-    if (startDate) newQuery.startDate = startDate
-    if (endDate) newQuery.endDate = endDate
-    if (organizationUnitId) newQuery.org = organizationUnitId
-    if (employeeId) newQuery.inCharge = employeeId
-    const res = await getCoutTask(newQuery)
-    if (res) { setReportTask(res) }
-    setLoading(false)
-  }
+    if (startDate) newQuery.startDate = startDate;
+    if (endDate) newQuery.endDate = endDate;
+    if (organizationUnitId) newQuery.org = organizationUnitId;
+    if (employeeId) newQuery.inCharge = employeeId;
+    const res = await getCoutTask(newQuery);
+    if (res) {
+      setReportTask(res);
+    }
+    setLoading(false);
+  };
 
   return (
     <>
       <View style={styles.view}>
-        <Button small rounded block 
-        style={{
-          width: '100%',
-          marginVertical: 2,  
-          borderRadius: 20, 
-          padding: 0 , 
-          margin: 0, 
-          backgroundColor: 'rgba(46, 149, 46, 1)', 
-          
-        }} 
-        
-        onPress={handleGetTask}>
-          <Text block style={{ justifyContent: 'center', alignItems: 'center', color:'white' , flex: 1, width:'100vw  ' }}>Công việc</Text>
-          <Icon type='Ionicons' name='reload' style={{ position: 'absolute', right: -140, color: '#fff' }} />
-        </Button> 
-      </View>   
+        <Button
+          small
+          rounded
+          block
+          style={{
+            width: '100%',
+            marginVertical: 2,
+            borderRadius: 20,
+            padding: 0,
+            margin: 0,
+            backgroundColor: 'rgba(46, 149, 46, 1)',
+          }}
+          onPress={handleGetTask}>
+          <Text
+            block
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              color: 'white',
+              flex: 1,
+              width: '100vw  ',
+            }}>
+            Công việc
+          </Text>
+          <Icon
+            type="Ionicons"
+            name="reload"
+            style={{position: 'absolute', right: -140, color: '#fff'}}
+          />
+        </Button>
+      </View>
       <LoadingLayout isLoading={loading}>
         {configs.map((config, index) => {
-          return <View style={styles.view} key={`CHKD_${index}`} >
-            {config.map((e) => (
-              <CustomButton key={e._id} kanban={e} reportTask={reportTask} />
-            ))}
-          </View>
-        })
-        }
+          return (
+            <View style={styles.view} key={`CHKD_${index}`}>
+              {config.map(e => (
+                <CustomButton key={e._id} kanban={e} reportTask={reportTask} />
+              ))}
+            </View>
+          );
+        })}
       </LoadingLayout>
     </>
   );
 };
 
-const CustomButton = (props) => {
-  const { kanban, reportTask } = props;
-  const [text, setText] = useState(0)
+const CustomButton = props => {
+  const {kanban, reportTask} = props;
+  const [text, setText] = useState(0);
   useEffect(() => {
     if (reportTask && Array.isArray(reportTask.data)) {
-      const obj = reportTask.data.find((item) => item.type === kanban.type)
+      const obj = reportTask.data.find(item => item.type === kanban.type);
 
       if (obj && obj.count) {
-        setText(obj.count)
+        setText(obj.count);
       }
     }
-  }, [kanban, reportTask])
+  }, [kanban, reportTask]);
 
-  const handleGotoPage = (params) => {
+  const handleGotoPage = params => {
     navigate('Project', params);
   };
   return (
-    <TouchableOpacity style={styles.button} onPress={() => handleGotoPage({ kanbanStatus: kanban.type })}>
-      <Text style={{ ...styles.textButton, color: kanban.color }}>
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => handleGotoPage({kanbanStatus: kanban.type})}>
+      <Text style={{...styles.textButton, color: kanban.color}}>
         {text || 0}
       </Text>
       <Text style={styles.textNote}>{kanban.name}</Text>
@@ -120,7 +136,6 @@ const CustomButton = (props) => {
 };
 const styles = {
   view: {
-    
     flex: 1,
     flexDirection: 'row',
   },
@@ -140,11 +155,24 @@ const styles = {
     margin: 3,
     height: 100,
     paddingHorizontal: 5,
-    // alignItems: 'center', 
+    // alignItems: 'center',
     // justifyContent: 'center'
   },
-  textButton: { color: 'black', marginBottom: 10, marginTop: 10, paddingTop: 8, fontSize: 24, fontWeight: 'bold', textAlign: 'center' },
-  textNote: { color: 'black', marginBottom: 10, fontSize: 10, textAlign: 'center' },
+  textButton: {
+    color: 'black',
+    marginBottom: 10,
+    marginTop: 10,
+    paddingTop: 8,
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  textNote: {
+    color: 'black',
+    marginBottom: 10,
+    fontSize: 10,
+    textAlign: 'center',
+  },
 
   textNote_2: {
     color: 'black',
