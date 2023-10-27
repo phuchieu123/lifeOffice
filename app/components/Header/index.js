da5f6155c9b480cee72922d001ff17906fecbb4e
MinhND <minh1996bk@gmail.com>
2023-10-27 11:29:05 +0700

    upgradate thu vien trong file index.js cua thu muc app/component/Header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,42 +1,44 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Header, Icon, Title, Body, Right, View, Text } from 'native-base';
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect';
-import _ from 'lodash';
-import MsgIcon from '../../containers/Message/components/MsgIcon';
+    import React, { useState, useEffect, useRef } from 'react';
+    import {  Body, Right, } from 'native-base';
+    import { connect } from 'react-redux'
+    import { createStructuredSelector } from 'reselect';
+    import { View, Text } from 'react-native';
+    import Icon from 'react-native-vector-icons/MaterialIcons';
+    import _ from 'lodash';
+    import MsgIcon from '../../containers/Message/components/MsgIcon';
 
-function CustomHeader(props) {
-    const { navigation, title, onGoBack, rightHeader } = props;
+    function CustomHeader(props) {
+        const { navigation, title, onGoBack, rightHeader } = props;
 
-    const handleGoBack = () => {
-        if (onGoBack) {
-            onGoBack();
-        } else {
-            navigation.goBack();
-        }
-    };
+        const handleGoBack = () => {
+            if (onGoBack) {
+                onGoBack();
+            } else {
+                navigation.goBack();
+            }
+        };
 
-    return (
-        <Header>
-            <Body style={{ flexDirection: 'row', flex: 1 }}>
-                {navigation && <Icon
-                    name="arrow-back"
-                    type="MaterialIcons"
-                    onPress={handleGoBack}
-                    style={{ color: '#fff', top: 3 }}
-                />}
-                <Title style={{ marginLeft: 10 }}>{title}</Title>
-            </Body>
-            <Right style={{ top: 2 }}>
-                {rightHeader}
-                {/* <MsgIcon /> */}
-            </Right>
-        </Header >
-    );
-}
+        return (
+            <View>
+                <View style={{ flexDirection: 'row', flex: 1 }}>
+                    {navigation && <Icon
+                        name="arrow-back"
+                        type="MaterialIcons"
+                        onPress={handleGoBack}
+                        style={{ color: '#fff', top: 3 }}
+                    />}
+                    <Text style={{ marginLeft: 10 }}>{title}</Text>
+                </View>
+                <View style={{ top: 2 }}>
+                    {rightHeader}
+                    {/* <MsgIcon /> */}
+                </View>
+            </View >
+        );
+    }
 
-// export default CustomHeader;
-export default connect(
-    createStructuredSelector({
-    }),
-)(CustomHeader);
+    // export default CustomHeader;
+    export default connect(
+        createStructuredSelector({
+        }),
+    )(CustomHeader);
